a5ec5335a7b8d6bd1142ebc2b87894afe5c49ab1
root <biniyamgetaneh30@gmail.com>
2022-01-11 03:42:24 -0800

    sort header file

--- sort.h
+++ sort.h
@@ -8,8 +8,8 @@
  * struct listint_s - Doubly linked list node
  *
  * @n: Integer stored in the node
- * @prev: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
+ * @prev: Ptr to the previous element of the list
+ * @next: Ptr to the next element of the list
  */
 typedef struct listint_s
 {
